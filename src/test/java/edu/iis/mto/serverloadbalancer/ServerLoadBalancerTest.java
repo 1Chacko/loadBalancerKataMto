c7befc826fb39a5c7ac12c006ee20920c6694eca
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-06-17 18:41:39 +0200

    filling server with vm implemented

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.serverloadbalancer;
 
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -14,11 +15,35 @@ public class ServerLoadBalancerTest {
 
 	@Test
 	public void balancingServerWithNoVms_serverStaysEmpty() {
-		Server theServer = a(ServerBuilder.server().withCapacity(1));
+		Server theServer = a(server().withCapacity(1));
 
 		balancing(aServersListWith(theServer), anEmptyListOfVms());
 
 		assertThat(theServer, CurrentLoadPercentageMatcher.hasCurrentLoadOf(0.0d));
+	}
+
+	@Test
+	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsServerWithTheVm() {
+		Server theServer = a(server().withCapacity(1));
+		
+		Vm theVm = a(vm().ofSize(1));
+		
+		balancing(aServersListWith(theServer), aVmsListWith(theVm));
+
+		assertThat(theServer, CurrentLoadPercentageMatcher.hasCurrentLoadOf(100.0d));
+		assertThat("server should contain the vm", theServer.contains(theVm));
+	}
+
+	private Vm[] aVmsListWith(Vm... vms) {
+		return vms;
+	}
+
+	private Vm a(VmBuilder builder) {
+		return builder.build();
+	}
+
+	private VmBuilder vm() {
+		return new VmBuilder();
 	}
 
 	private void balancing(Server[] servers, Vm[] vms) {
