9d2df7523391a315813bc2cac980446548d7f46d
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-06-19 19:23:56 +0200

    test 1 refactorization

--- src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -6,6 +6,7 @@ import org.hamcrest.TypeSafeMatcher;
 
 public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
 
+	private static final double EPSILON = 0.01d;
 	private double expectedLoadPercentage;
 
 	public CurrentLoadPercentageMatcher(double expectedLoadPercentage) {
@@ -18,8 +19,16 @@ public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
 
 	@Override
 	protected boolean matchesSafely(Server server) {
-		return expectedLoadPercentage == server.currentLoadPercentage
-				|| Math.abs(expectedLoadPercentage - server.currentLoadPercentage) < 0.01d;
+		return doublesAreEqual(expectedLoadPercentage, server.currentLoadPercentage);
+	}
+
+	private boolean doublesAreEqual(double d1, double d2) {
+		return d1 == d2
+				|| Math.abs(d1 - d2) < EPSILON;
+	}
+
+	public static CurrentLoadPercentageMatcher hasCurrentLoadOf(double expectedLoadPercentage) {
+		return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
 	}
 
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,9 +1,10 @@
 package edu.iis.mto.serverloadbalancer;
 
+import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasCurrentLoadOf;
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -21,10 +22,6 @@ public class ServerLoadBalancerTest {
 		assertThat(theServer, hasCurrentLoadOf(0.0d));
 	}
 
-	private Matcher<? super Server> hasCurrentLoadOf(double expectedLoadPercentage) {
-		return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
-	}
-
 	private void balancing(Server[] servers, Vm[] vms) {
 		new ServerLoadBalancer().balance(servers, vms);
 	}
@@ -39,10 +36,6 @@ public class ServerLoadBalancerTest {
 
 	private Server a(ServerBuilder builder) {
 		return builder.build();
-	}
-
-	private ServerBuilder server() {
-		return new ServerBuilder();
 	}
 
 }
