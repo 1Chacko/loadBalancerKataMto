edd49d77efb6afa738ea5d1d85f8bd4659e0a7e0
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-06-20 14:17:42 +0200

    test 4 green

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,5 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hamcrest.Matcher;
 
 public class Server {
@@ -7,6 +10,8 @@ public class Server {
 	private static final double MAXIMUM_LOAD = 100.0d;
 	public double currentLoadPercentage;
 	public int capacity;
+	
+	private List<Vm> vms = new ArrayList<Vm>();
 
 	public boolean contains(Vm theVm) {
 		return true;
@@ -18,7 +23,12 @@ public class Server {
 	}
 
 	public void addVm(Vm vm) {
-		currentLoadPercentage = (double)vm.size / (double)capacity * MAXIMUM_LOAD;		
+		currentLoadPercentage = (double)vm.size / (double)capacity * MAXIMUM_LOAD;
+		vms.add(vm);
+	}
+
+	public int countVms() {
+		return vms.size();
 	}
 
 }
